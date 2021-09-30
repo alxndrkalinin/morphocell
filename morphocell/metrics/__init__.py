f8d91c020b719ad6ed2a7005240894a91659789c
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-09-30 14:17:43 +0000

    metrics: expose resolution difference metric

--- morphocell/metrics/__init__.py
+++ morphocell/metrics/__init__.py
@@ -1,5 +1,5 @@
 """Expose metrics functions."""
 
-from .frc import calculate_frc, five_crop_resolution, grid_crop_resolution
+from .frc import calculate_frc, five_crop_resolution, grid_crop_resolution, frc_resolution_difference
 
-__all__ = ["calculate_frc", "five_crop_resolution", "grid_crop_resolution"]
+__all__ = ["calculate_frc", "five_crop_resolution", "grid_crop_resolution", "frc_resolution_difference"]
