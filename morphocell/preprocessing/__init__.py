f30148a79e9daf5571caba58931c52c52c4560af
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-10-08 18:43:56 +0300

    preprocess: expose patch selection

--- morphocell/preprocessing/__init__.py
+++ morphocell/preprocessing/__init__.py
@@ -1,5 +1,12 @@
 """Expose preprocessing functions."""
 
 from .deconvolution import richardson_lucy_dl2, richardson_lucy_flowdec, decon_flowdec, decon_iter_num_finder
+from .thresholding import select_binary_patches
 
-__all__ = ["richardson_lucy_dl2", "richardson_lucy_flowdec", "decon_flowdec", "decon_iter_num_finder"]
+__all__ = [
+    "richardson_lucy_dl2",
+    "richardson_lucy_flowdec",
+    "decon_flowdec",
+    "decon_iter_num_finder",
+    "select_binary_patches",
+]
