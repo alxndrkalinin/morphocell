d702536220247f7cce864dafcc853b5f67100955
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-10-21 19:17:27 +0300

    image: WIP allow specifying device on Image creation

--- morphocell/image.py
+++ morphocell/image.py
@@ -1,28 +1,39 @@
 """Contains a simple class for storing image data."""
 
+import numpy as np
+
 try:
     from cupy.cuda.runtime import getDeviceCount
 
     if getDeviceCount() > 0:
-        device_name = "GPU"
         import cupy as xp
+
+        device_name = "GPU"
+        asnumpy = xp.asnumpy
     else:
         raise
 except Exception:
     device_name = "CPU"
-    import numpy as xp
+    xp = np
+    asnumpy = np.asarray
 
 
 class Image(xp.ndarray):
     """A very simple extension to numpy/cupy ndarray, to contain image data and metadata."""
 
-    def __new__(cls, images, spacing, filename=None):
+    def __new__(cls, images, spacing, filename=None, device="CPU"):
         """Create new Image object with metadata."""
-        obj = xp.asarray(images).view(cls)
+        if device == "GPU" and device_name == "GPU":
+            obj = xp.asarray(images).view(cls)
+        elif device == "CPU" and device_name == "GPU":
+            obj = asnumpy(xp.asarray(images).view(cls))
+        else:
+            obj = np.asarray(images).view(cls)
+            device = "CPU"
 
         obj.spacing = list(spacing)
         obj.filename = filename
-        obj.device = device_name
+        obj.device = device
 
         return obj
 
@@ -31,3 +42,4 @@ class Image(xp.ndarray):
         """Finalize the subclass of ndarray."""
         self.spacing = getattr(obj, "spacing")
         self.filename = getattr(obj, "filename", None)
+        self.device = getattr(obj, "device", "CPU")
