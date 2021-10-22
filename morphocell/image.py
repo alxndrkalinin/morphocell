c0cbafc73710976903e43b8ff82a32d6ccf09ad0
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-10-22 19:52:45 +0300

    image: add method for moving data to GPU

--- morphocell/image.py
+++ morphocell/image.py
@@ -9,19 +9,18 @@ from .gpu import get_gpu_info, get_device
 class Image:
     """A simple class to store NumPy/CuPy image data and metadata."""
 
+    gpu_info = get_gpu_info()
+
     def __init__(self, images, spacing: Sequence, filename: Optional[str] = None, device: Optional[str] = None):
         """Create image object with data stored either as NumPy array (CPU) or CuPy array (GPU)."""
-        gpu_info = get_gpu_info()
-
         if device is None:
             device = get_device(images)
         else:
             assert device in ["CPU", "GPU"]
 
-        if device == "GPU" and gpu_info.num_gpus > 0:
-            img_as_float32 = gpu_info.cucim.skimage.util.img_as_float32
-            self.data = img_as_float32(gpu_info.cp.asarray(images))
-        elif device == "GPU" and gpu_info.num_gpus == 0:
+        if device == "GPU" and self.gpu_info.num_gpus > 0:
+            self.data = self._to_gpu(images)
+        elif device == "GPU" and self.gpu_info.num_gpus == 0:
             print("\n GPU requested, but is not available! Creating Image on CPU.")
             self.data = np.asarray(images)
             device = "CPU"
@@ -32,3 +31,16 @@ class Image:
         self.spacing = tuple(spacing)
         self.filename = filename
         self.device = device
+
+    def _to_gpu(self, data):
+        """Move given array to GPU."""
+        img_as_float32 = self.gpu_info.cucim.skimage.util.img_as_float32
+        return img_as_float32(self.gpu_info.cp.asarray(data))
+
+    def to_gpu(self):
+        """Move Image data to GPU."""
+        if self.gpu_info.num_gpus > 0:
+            self.data = self._to_gpu(self.data)
+        else:
+            print("\n GPU requested, but is not available! Creating Image on CPU.")
+            raise ImportError
