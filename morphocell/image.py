2d505cdae2fc62f60bebc8483eec1748ea821ac0
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-10-22 03:35:51 +0300

    image,utils: WIP use decorator to run image ops either on GPU or CPU

--- morphocell/image.py
+++ morphocell/image.py
@@ -1,32 +1,27 @@
 """Contains a simple class for storing image data."""
 
+from typing import Optional, Sequence
 import numpy as np
-
-try:
-    import cupy as cp
-
-    if cp.cuda.runtime.getDeviceCount() > 0:
-        device_name = "GPU"
-    else:
-        raise
-except Exception:
-    device_name = "CPU"
+from .utils import Device
 
 
 class Image:
     """A very simple extension to numpy/cupy ndarray, to contain image data and metadata."""
 
-    def __init__(self, images, spacing, filename=None, device="CPU"):
-        """Create image object on specified device, if vaailable."""
-        if device == "GPU" and device_name == "GPU":
-            self.data = cp.asarray(images)
-        elif device == "GPU" and device_name == "CPU":
-            print("\n GPU is not available! Creating Image on CPU.")
-            device = device_name
+    def __init__(self, images, spacing: Sequence, filename: Optional[str] = None, device: str = "CPU"):
+        """Create image object on specified device, if available."""
+        device_config = Device()
+
+        if device == "GPU" and device_config.gpu:
+            self.data = device_config.xp.asarray(images)
+        elif device == "GPU" and not device_config.gpu:
+            print("\n GPU requested, but is not available! Creating Image on CPU.")
             self.data = np.asarray(images)
+            device = "CPU"
         else:
             self.data = np.asarray(images)
 
+        self.shape = self.data.shape
         self.spacing = tuple(spacing)
         self.filename = filename
         self.device = device
--- morphocell/image_utils.py
+++ morphocell/image_utils.py
@@ -4,55 +4,58 @@ from typing import Tuple, Union, List, Dict, Sequence, Optional
 import numpy.typing as npt
 
 import numpy as np
+
 from .image import Image
-
-try:
-    from cupy.cuda.runtime import getDeviceCount
-
-    if getDeviceCount() > 0:
-        device_name = "GPU"
-        import cupy as xp
-        from cucim.skimage.transform import rescale
-        from cucim.skimage.exposure import rescale_intensity
-
-    else:
-        raise
-except Exception:
-    device_name = "CPU"
-    xp = np
-    from skimage.transform import rescale
-    from skimage.exposure import rescale_intensity
+from .utils import gpuable, cpu_deps
 
 # image operations assume ZYX channel order
 
 
-def image_stats(img: npt.ArrayLike, q: Tuple[float, float] = (0.1, 99.9)) -> Dict[str, float]:
+@gpuable
+def image_stats(
+    img: npt.ArrayLike,
+    q: Tuple[float, float] = (0.1, 99.9),
+    deps: Optional[Dict] = None,
+) -> Dict[str, float]:
     """Compute intensity image statistics (min, max, mean, percentiles)."""
-    q_min, q_max = xp.percentile(img, q=q)
+    deps = deps if deps is not None else cpu_deps()
+    q_min, q_max = deps["xp"].percentile(img, q=q)
     return {
-        "min": xp.min(img),
-        "max": xp.max(img),
-        "mean": xp.mean(img),
+        "min": deps["xp"].min(img),
+        "max": deps["xp"].max(img),
+        "mean": deps["xp"].mean(img),
         "percentile_min": q_min,
         "precentile_max": q_max,
     }
 
 
-def rescale_xy(img: npt.ArrayLike, factor: float = 1.0, anti_aliasing=True) -> xp.ndarray:
+@gpuable
+def rescale_xy(
+    img: npt.ArrayLike,
+    factor: float = 1.0,
+    anti_aliasing=True,
+    deps: Optional[Dict] = None,
+):
     """Rescale image in XY."""
-    return rescale(img, (1.0, factor, factor), preserve_range=False, anti_aliasing=anti_aliasing)
+    deps = deps if deps is not None else cpu_deps()
+    return deps["xkimage"].transform.rescale(
+        img, (1.0, factor, factor), preserve_range=False, anti_aliasing=anti_aliasing
+    )
 
 
+@gpuable
 def rescale_isotropic(
     img: npt.ArrayLike,
     voxel_sizes: Union[Tuple[int, ...], Tuple[float, ...]],
     downscale_xy: bool = False,
     order: int = 3,
     target_z_size: Optional[int] = None,
-) -> xp.ndarray:
+    deps: Optional[Dict] = None,
+) -> npt.ArrayLike:
     """Rescale image to isotropic voxels with arbitary Z size."""
-    z_size_per_spacing = (img.shape[0] * voxel_sizes[0] / np.asarray(voxel_sizes)).astype(int)
+    deps = deps if deps is not None else cpu_deps()
 
+    z_size_per_spacing = (img.shape[0] * voxel_sizes[0] / np.asarray(voxel_sizes)).astype(int)
     if target_z_size is None:
         if downscale_xy:
             target_z_size = img.shape[0]
@@ -60,51 +63,80 @@ def rescale_isotropic(
             target_z_size = z_size_per_spacing[1]
 
     factors = target_z_size / z_size_per_spacing
-    return rescale(img, factors, order=order, preserve_range=True, anti_aliasing=downscale_xy)
+    return deps["xkimage"].transform.rescale(
+        img, factors, order=order, preserve_range=True, anti_aliasing=downscale_xy
+    )
 
 
-def normalize_min_max(img: npt.ArrayLike, q: Tuple[float, float] = (0.1, 99.9)) -> xp.ndarray:
+@gpuable
+def normalize_min_max(
+    img: npt.ArrayLike,
+    q: Tuple[float, float] = (0.1, 99.9),
+    deps: Optional[Dict] = None,
+):
     """Normalize image intensities between percentiles."""
-    vmin, vmax = xp.percentile(img, q=q)
-    return rescale_intensity(img, in_range=(vmin, vmax), out_range=xp.float32)
+    deps = deps if deps is not None else cpu_deps()
+
+    vmin, vmax = deps["xp"].percentile(img, q=q)
+    return deps["xkimage"].exposure.rescale_intensity(img, in_range=(vmin, vmax), out_range=np.float32)
 
 
-def max_project(img: npt.ArrayLike, axis: int = 0) -> xp.ndarray:
+@gpuable
+def max_project(
+    img: npt.ArrayLike,
+    axis: int = 0,
+    deps: Optional[Dict] = None,
+):
     """Compute maximum intensity projection along the chosen axis."""
-    return xp.max(img, axis)
+    deps = deps if deps is not None else cpu_deps()
+    return deps["xp"].max(img, axis)
 
 
-def img_mse(a, b) -> int:
+@gpuable
+def img_mse(
+    a,
+    b,
+    deps: Optional[Dict] = None,
+) -> int:
     """Calculate pixel-wise MSE between two images."""
+    deps = deps if deps is not None else cpu_deps()
     assert len(a) == len(b)
-    return xp.square(a - b).mean()
+    return deps["xp"].square(a - b).mean()
 
 
+@gpuable
 def pad_image(
     img: npt.ArrayLike,
     pad_size: Union[int, Sequence[int]],
     axis: int = 0,
     mode: str = "reflect",
-) -> xp.ndarray:
+    deps: Optional[Dict] = None,
+):
     """Pad an image."""
+    deps = deps if deps is not None else cpu_deps()
+
     npad = np.asarray([(0, 0)] * img.ndim)
     npad[axis] = [pad_size] * 2 if isinstance(pad_size, int) else pad_size
-    return xp.pad(img, pad_width=npad, mode=mode)
+    return deps["xp"].pad(img, pad_width=npad, mode=mode)
 
 
+@gpuable
 def pad_image_cpu(
     img: npt.ArrayLike,
     pad_size: Union[int, Sequence[int]],
     axis: int = 0,
     mode: str = "reflect",
-) -> xp.ndarray:
+    deps: Optional[Dict] = None,
+):
     """Pad an image on a CPU."""
+    deps = deps if deps is not None else cpu_deps()
+
     npad = np.asarray([(0, 0)] * img.ndim)
     npad[axis] = [pad_size] * 2 if isinstance(pad_size, int) else pad_size
     return np.pad(img, pad_width=npad, mode=mode)
 
 
-def pad_image_to_cube(img: npt.ArrayLike, cube_size: int, mode: str = "reflect") -> xp.ndarray:
+def pad_image_to_cube(img: npt.ArrayLike, cube_size: int, mode: str = "reflect"):
     """Pad all image axis up to cubic shape."""
     for i, dim in enumerate(img.shape):
         if dim < cube_size:
@@ -115,7 +147,7 @@ def pad_image_to_cube(img: npt.ArrayLike, cube_size: int, mode: str = "reflect")
     return img
 
 
-def pad_image_to_shape(img: npt.ArrayLike, new_shape: Sequence, mode: str = "constant") -> xp.ndarray:
+def pad_image_to_shape(img: npt.ArrayLike, new_shape: Sequence, mode: str = "constant"):
     """Pad all image axis up to specified shape."""
     for i, dim in enumerate(img.shape):
         if dim < new_shape[i]:
@@ -141,31 +173,31 @@ def pad_images_to_matching_shape(image1: npt.ArrayLike, image2: npt.ArrayLike, m
     return image1, image2
 
 
-def crop_tl(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]) -> xp.ndarray:
+def crop_tl(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]):
     """Crop left top corner."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
     return img[:, :crop_h, :crop_w]
 
 
-def crop_bl(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]) -> xp.ndarray:
+def crop_bl(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]):
     """Crop left bottom corner."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
     return img[:, -crop_h:, :crop_w]
 
 
-def crop_tr(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]) -> xp.ndarray:
+def crop_tr(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]):
     """Crop right top corner."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
     return img[:, :crop_h, -crop_w:]
 
 
-def crop_br(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]) -> xp.ndarray:
+def crop_br(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]):
     """Crop right bottom corner."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
     return img[:, -crop_h:, -crop_w:]
 
 
-def crop_center(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]) -> xp.ndarray:
+def crop_center(img: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]):
     """Crop from the center of the image."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
 
@@ -202,7 +234,7 @@ def random_crop(
     img: npt.ArrayLike,
     crop_hw: Union[int, Tuple[int, int]],
     return_coordinates: bool = False,
-) -> xp.ndarray:
+):
     """Crop from a random location in the image."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
     height, width = img.shape[1:]
@@ -214,7 +246,7 @@ def random_crop(
         return img[y1:y2, x1:x2]
 
 
-def get_xy_block_coords(image_shape: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]) -> xp.ndarray:
+def get_xy_block_coords(image_shape: npt.ArrayLike, crop_hw: Union[int, Tuple[int, int]]):
     """Compute coordinates of non-overlapping image blocks of specified shape."""
     crop_h, crop_w = (crop_hw, crop_hw) if isinstance(crop_hw, int) else crop_hw
     height, width = image_shape[1:]
@@ -227,12 +259,12 @@ def get_xy_block_coords(image_shape: npt.ArrayLike, crop_hw: Union[int, Tuple[in
     return np.asarray(block_coords).astype(int)
 
 
-def get_xy_block(image: npt.ArrayLike, patch_coordinates: List[int]) -> xp.ndarray:
+def get_xy_block(image: npt.ArrayLike, patch_coordinates: List[int]):
     """Slice subvolume of 3D image by XY coordinates."""
     return image[:, patch_coordinates[0] : patch_coordinates[1], patch_coordinates[2] : patch_coordinates[3]]
 
 
-def extract_patches(image: npt.ArrayLike, patch_coordinates: List[List[int]]) -> List[xp.ndarray]:
+def extract_patches(image: npt.ArrayLike, patch_coordinates: List[List[int]]):
     """Extract 3D patches from image given XY coordinates."""
     patches = []
     for patch_coords in patch_coordinates:
@@ -240,7 +272,7 @@ def extract_patches(image: npt.ArrayLike, patch_coordinates: List[List[int]]) ->
     return patches
 
 
-def _nd_window(data, filter_function, **kwargs):
+def _nd_window(data, filter_function, power_function, **kwargs):
     """
     Perform on N-dimensional spatial-domain data to mitigate boundary effects in the FFT.
 
@@ -261,13 +293,18 @@ def _nd_window(data, filter_function, **kwargs):
         filter_shape[axis] = axis_size
         window = filter_function(axis_size, **kwargs).reshape(filter_shape)
         # scale the window intensities to maintain array intensity
-        xp.power(window, (1.0 / data.ndim), out=window)
+        power_function(window, (1.0 / data.ndim), out=window)
         result *= window
     return result
 
 
-def hamming_window(data):
+@gpuable
+def hamming_window(
+    data,
+    deps: Optional[Dict] = None,
+):
     """Apply Hamming window to data."""
-    assert issubclass(data.__class__, xp.ndarray)
+    deps = deps if deps is not None else cpu_deps()
+    assert issubclass(data.__class__, deps["xp"].ndarray)
 
-    return _nd_window(data, xp.hamming)
+    return _nd_window(data, deps["xp"].hamming, deps["xp"].power)
