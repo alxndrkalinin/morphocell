6e2832a8444c4df4eddd9d697204ec18b4b6852c
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-11-26 14:05:06 +0000

    thresholding: allow precomputed threshold for patch selection

--- morphocell/preprocessing/thresholding.py
+++ morphocell/preprocessing/thresholding.py
@@ -1,6 +1,6 @@
 """Implement 3D image thresholding."""
 
-from typing import List
+from typing import List, Optional
 import numpy.typing as npt
 
 import numpy as np
@@ -10,27 +10,32 @@ from ..image_utils import get_xy_block_coords, get_xy_block
 from ..gpu import get_image_method
 
 
-def threshold(image: npt.ArrayLike, blur_sigma=5):
+def get_threshold_otsu(image: npt.ArrayLike, blur_sigma=5):
     """Perform Otsu's thresholding with Gaussian blur."""
     skimage_gaussian = get_image_method(image, "skimage.filters.gaussian")
     skimage_otsu = get_image_method(image, "skimage.filters.threshold_otsu")
     image = skimage_gaussian(image, sigma=blur_sigma)
-    thresh = skimage_otsu(image)
-    binary = image > thresh
-    return binary
+    return skimage_otsu(image)
 
 
 def select_nonempty_patches(
     image: npt.ArrayLike,
     patch_size: int = 512,
     min_nonzeros: float = 0.02,
+    threshold: Optional[float] = None,
     verbose: bool = False,
 ) -> List[List[int]]:
     """Select XY patches from 3D image by percent of nonzero voxels."""
     verboseprint = print if verbose else lambda *a, **k: None
 
     selected_patches = []
-    binary_image = threshold(image)
+
+    if threshold is None:
+        threshold = get_threshold_otsu(image)
+
+    img_as_float = get_image_method(image, "skimage.img_as_float")
+    binary_image = (img_as_float(image) > threshold).astype(np.uint8)
+
     patch_coordinates = get_xy_block_coords(image.shape, patch_size)
     verboseprint(f"Nonzero pixels in the image: {np.count_nonzero(binary_image) / binary_image.size}")
 
