45a9992daf7032c8ce30a3411902f79f5316c9c9
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-12-16 15:08:55 +0000

    __init__: expose more preprocess & segment funcs

--- morphocell/preprocessing/__init__.py
+++ morphocell/preprocessing/__init__.py
@@ -1,7 +1,7 @@
 """Expose preprocessing functions."""
 
 from .deconvolution import richardson_lucy_dl2, richardson_lucy_flowdec, decon_flowdec, decon_iter_num_finder
-from .thresholding import select_nonempty_patches
+from .thresholding import select_nonempty_patches, get_threshold_otsu
 
 __all__ = [
     "richardson_lucy_dl2",
@@ -9,4 +9,5 @@ __all__ = [
     "decon_flowdec",
     "decon_iter_num_finder",
     "select_nonempty_patches",
+    "get_threshold_otsu",
 ]
--- morphocell/segmentation/__init__.py
+++ morphocell/segmentation/__init__.py
@@ -1,10 +1,6 @@
 """Expose preprocessing functions."""
 
 from .cellpose import cellpose_segment
-from .segment_utils import downsample_and_filter, remove_touching_objects
+from .segment_utils import downsample_and_filter, remove_touching_objects, remove_small_objects
 
-__all__ = [
-    "cellpose_segment",
-    "downsample_and_filter",
-    "remove_touching_objects",
-]
+__all__ = ["cellpose_segment", "downsample_and_filter", "remove_touching_objects", "remove_small_objects"]
