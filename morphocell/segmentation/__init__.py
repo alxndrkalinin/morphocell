fccc918789dd4fe0d44d12a06987f3efaf75dc51
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2022-02-17 13:17:09 +0000

    segment: expose clear_xy_borders

--- morphocell/segmentation/__init__.py
+++ morphocell/segmentation/__init__.py
@@ -1,6 +1,12 @@
 """Expose preprocessing functions."""
 
 from .cellpose import cellpose_segment
-from .segment_utils import downsample_and_filter, remove_touching_objects, remove_small_objects
+from .segment_utils import downsample_and_filter, remove_touching_objects, remove_small_objects, clear_xy_borders
 
-__all__ = ["cellpose_segment", "downsample_and_filter", "remove_touching_objects", "remove_small_objects"]
+__all__ = [
+    "cellpose_segment",
+    "downsample_and_filter",
+    "remove_touching_objects",
+    "remove_small_objects",
+    "clear_xy_borders",
+]
