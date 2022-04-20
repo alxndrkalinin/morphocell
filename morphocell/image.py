cc3f8fe822fecd23e93e4031da18ecb12a7d3278
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2022-04-20 21:22:23 +0000

    image: make conversion to float optional

--- morphocell/image.py
+++ morphocell/image.py
@@ -11,7 +11,14 @@ class Image:
 
     gpu_info = get_gpu_info()
 
-    def __init__(self, images, spacing: Sequence, filename: Optional[str] = None, device: Optional[str] = None):
+    def __init__(
+        self,
+        images,
+        spacing: Sequence,
+        filename: Optional[str] = None,
+        device: Optional[str] = None,
+        as_float: bool = True,
+    ):
         """Create image object with data stored either as NumPy array (CPU) or CuPy array (GPU)."""
         if device is None:
             device = get_device(images)
@@ -19,7 +26,7 @@ class Image:
             assert device in ["CPU", "GPU"]
 
         if device == "GPU" and self.gpu_info["num_gpus"] > 0:
-            self.data = self._to_gpu(images)
+            self.data = self._to_gpu(images, as_float=as_float)
         elif device == "GPU" and self.gpu_info["num_gpus"] == 0:
             print("\n GPU requested, but is not available! Creating Image on CPU.")
             self.data = np.asarray(images)
@@ -32,16 +39,18 @@ class Image:
         self.filename = filename
         self.device = device
 
-    def _to_gpu(self, data):
+    def _to_gpu(self, data, as_float: bool = True):
         """Move given array to GPU."""
         data = self.gpu_info["cp"].asarray(data)
-        img_as_float32 = get_image_method(data, "skimage.util.img_as_float32")
-        return img_as_float32(data)
+        if as_float:
+            img_as_float32 = get_image_method(data, "skimage.util.img_as_float32")
+            data = img_as_float32(data)
+        return data
 
-    def to_gpu(self):
+    def to_gpu(self, as_float: bool = True):
         """Move Image data to GPU."""
         if self.gpu_info["num_gpus"] > 0:
-            self.data = self._to_gpu(self.data)
+            self.data = self._to_gpu(self.data, as_float=as_float)
             self.device = "GPU"
         else:
             print("\n GPU requested, but is not available! Creating Image on CPU.")
