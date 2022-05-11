89aa435afaa1e98cae25e1c41eafeac9f9f4db75
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2022-05-11 04:15:34 +0000

    voxel_feats: tmp link to regionprops_table fix PR to CuCIM

--- morphocell/feature/voxel_morphometry.py
+++ morphocell/feature/voxel_morphometry.py
@@ -7,11 +7,13 @@ from ..gpu import get_image_method
 
 
 def regionprops(image_bin: npt.ArrayLike, image: npt.ArrayLike, features: Optional[List] = None):
-    """Perform Otsu's thresholding with Gaussian blur."""
+    """Extract region-based morphological features."""
     features = [] if features is None else features
 
     skimage_label = get_image_method(image, "skimage.measure.label")
     skimage_regionprops_table = get_image_method(image, "skimage.measure.regionprops_table")
 
     label_image_bin = skimage_label(image_bin)
+
+    # this will fail on GPU if any properties return objects, see https://github.com/rapidsai/cucim/pull/272
     return skimage_regionprops_table(label_image_bin, image, properties=["label"] + features)
