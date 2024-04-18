a82c89b0e9990951eaf41a440b1a279ab106246e
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2024-04-18 22:43:46 +0000

    refactor(feature): make intensity image optional in regionprops

--- morphocell/feature/voxel_morphometry.py
+++ morphocell/feature/voxel_morphometry.py
@@ -6,14 +6,10 @@ import numpy.typing as npt
 from ..gpu import get_image_method
 
 
-def regionprops(image_bin: npt.ArrayLike, image: npt.ArrayLike, features: Optional[List] = None):
+def regionprops(label_image: npt.ArrayLike, image: Optional[npt.ArrayLike] = None, features: Optional[List] = None):
     """Extract region-based morphological features."""
     features = [] if features is None else features
+    skimage_regionprops_table = get_image_method(label_image, "skimage.measure.regionprops_table")
 
-    skimage_label = get_image_method(image, "skimage.measure.label")
-    skimage_regionprops_table = get_image_method(image, "skimage.measure.regionprops_table")
-
-    label_image_bin = skimage_label(image_bin)
-
-    # this will fail on GPU if any properties return objects, see https://github.com/rapidsai/cucim/pull/272
-    return skimage_regionprops_table(label_image_bin, image, properties=["label"] + features)
+    # this will return results on the same device as the input label_image
+    return skimage_regionprops_table(label_image, image, properties=["label"] + features)
