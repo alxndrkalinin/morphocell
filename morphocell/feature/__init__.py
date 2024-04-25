b17bd247f4146feca864545349f841af59d27ed3
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2024-04-25 02:09:30 +0000

    refactor(feature): keep both regionprops/regionprops_table, add precentile calculation

--- morphocell/feature/__init__.py
+++ morphocell/feature/__init__.py
@@ -1,7 +1,8 @@
 """Expose feature extraction functions."""
 
-from .voxel_morphometry import regionprops
+from .voxel_morphometry import regionprops_table
 
 __all__ = [
     "regionprops",
+    "regionprops_table",
 ]
--- morphocell/feature/voxel_morphometry.py
+++ morphocell/feature/voxel_morphometry.py
@@ -1,15 +1,48 @@
 """Implement extraction of 3D voxel-based morphological features."""
 
-from typing import List, Optional
+from typing import List, Tuple, Optional
 import numpy.typing as npt
+
+import numpy as np
 
 from ..gpu import get_image_method
 
 
-def regionprops(label_image: npt.ArrayLike, image: Optional[npt.ArrayLike] = None, features: Optional[List] = None):
+def regionprops(
+    label_image: npt.ArrayLike, intensity_image: Optional[npt.ArrayLike] = None, spacing: Optional[List] = None
+):
     """Extract region-based morphological features."""
-    features = [] if features is None else features
-    skimage_regionprops_table = get_image_method(label_image, "skimage.measure.regionprops_table")
+    skimage_regionprops = get_image_method(label_image, "skimage.measure.regionprops")
+    return skimage_regionprops(label_image, intensity_image, spacing=spacing)
 
-    # this will return results on the same device as the input label_image
-    return skimage_regionprops_table(label_image, image, properties=["label"] + features)
+
+def regionprops_table(
+    label_image: npt.ArrayLike,
+    intensity_image: Optional[npt.ArrayLike] = None,
+    properties: Optional[List] = None,
+    spacing: Optional[List] = None,
+):
+    """Extract region-based morphological features and return in pandas-compatible format."""
+    if properties is not None:
+        properties = properties if "label" in properties else properties + ["label"]
+    else:
+        properties = []
+    skimage_regionprops_table = get_image_method(label_image, "skimage.measure.regionprops_table")
+    return skimage_regionprops_table(label_image, intensity_image, properties=properties)
+
+
+def calculate_feature_percentiles(
+    regionprops_dict: dict,
+    percentiles: Tuple = (0.01, 0.99),
+    features_include: Optional[list] = None,
+    features_exclude: Optional[list] = None,
+):
+    """Calculate percentiles of features in regionprops."""
+    percentiles = np.asarray(percentiles) * 100
+    if features_include is not None:
+        features = [f for f in features_include if f in regionprops_dict]
+    else:
+        features = list(regionprops_dict.keys())
+    if features_exclude is not None:
+        features = [f for f in features if f not in features_exclude]
+    return {f: np.percentile(regionprops_dict[f], percentiles) for f in features if f in regionprops_dict}
