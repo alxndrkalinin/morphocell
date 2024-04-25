2c653a701c14d4a81fd913a6a2de0f9de9ae8eba
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2024-04-25 02:19:58 +0000

    fix(feature): actually expose regioprops in features

--- morphocell/feature/__init__.py
+++ morphocell/feature/__init__.py
@@ -1,6 +1,6 @@
 """Expose feature extraction functions."""
 
-from .voxel_morphometry import regionprops_table
+from .voxel_morphometry import regionprops, regionprops_table
 
 __all__ = [
     "regionprops",
